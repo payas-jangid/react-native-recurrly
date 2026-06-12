395777b2187485b0b3676d8d0c4f07290b32193c
= <=>
2026-06-12 18:23:43 +0530

    implemented posthog

--- lib/auth.ts
+++ lib/auth.ts
@@ -172,7 +172,7 @@ export const debounce = <T extends (...args: any[]) => any>(
   func: T,
   delay: number,
 ): ((...args: Parameters<T>) => void) => {
-  let timeoutId: NodeJS.Timeout;
+  let timeoutId: ReturnType<typeof setTimeout>;
 
   return (...args: Parameters<T>) => {
     clearTimeout(timeoutId);
